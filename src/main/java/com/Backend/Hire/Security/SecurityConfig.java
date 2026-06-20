20ee6ca52518665b111891b11b5d4d188eb3c1c0
SantoshSR14 <sec23cs175@sairamtap.edu.in>
2026-06-20 23:51:38 +0530

    Update SecurityConfig.java

--- src/main/java/com/Backend/Hire/Security/SecurityConfig.java
+++ src/main/java/com/Backend/Hire/Security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
